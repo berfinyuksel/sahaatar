b59842f60fee8e3b00f911008cd691143601f63d
berfinyuksel <berfin.yuksell@gmail.com>
2023-11-05 14:32:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import psycopg2
+
+# Connect to the database
+conn = psycopg2.connect(
+    host="your_host",
+    database="your_database",
+    user="your_user",
+    password="your_password"
+)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM your_table")
+
+# Fetch the results
+results = cur.fetchall()
+
+# Print the results
+for row in results:
+    print(row)
+
+# Close the cursor and connection
+cur.close()
+conn.close()
 
